87d51d13f8a100b2f2c2d38a297a4482f1d4e2ff
Yashas Samaga <yashas224@gmail.com>
2026-07-18 19:58:29 +0000

    Generating JWT token after user authenticating

--- routers/auth.py
+++ routers/auth.py
@@ -1,10 +1,23 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel, Field
-from models import Users
+from sqlalchemy import select
+import jwt
+import os
 from dependencies import db_dependency
+from models import Users
 from utils import password_util
 
 router = APIRouter()
+
+load_dotenv()
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 class CreateUserRequest(BaseModel):
@@ -14,6 +27,35 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+                      , db_session: db_dependency):
+    username = form_data.username
+    password = form_data.password
+    stmt = select(Users).where(Users.username == username)
+    user: Users = db_session.scalars((stmt)).first()
+    if user:
+        is_valid_password = password_util.verify_password(password, user.hashed_password)
+        if is_valid_password:
+            return user
+    else:
+        return None
+
+    return None
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
@@ -31,3 +73,22 @@ async def create_user(create_user_req: CreateUserRequest
     db_session.add(user_model)
     db_session.commit()
     return {"message": "User Created "}
+
+
+@router.post("/token", response_model=Token, status_code=status.HTTP_200_OK)
+async def get_token(authenticate_user: Annotated[Users, Depends(authenticate_user)]):
+    if authenticate_user:
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": authenticate_user.username, "id": authenticate_user.id}, expires_delta=access_token_expires
+        )
+        return Token(
+            access_token=access_token,
+            token_type="bearer"
+        )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
