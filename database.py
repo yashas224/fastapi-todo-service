bc54905b579480ef669792f20aa41acdae0d75a6
Yashas Samaga <yashas224@gmail.com>
2026-07-19 11:48:03 +0000

    integrating with PostgreSQL

--- database.py
+++ database.py
@@ -1,10 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+import os
 
-SQL_ALCHEMY_DATABASE_URL = "sqlite:///./todo-service.db"
+sqlite = "SQLite"
+postgres = "PostgreSQL"
+load_dotenv()
+SQL_ALCHEMY_DATABASE_URL = ""
+engine = None
 
-engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False, "timeout": 30}, echo=True)
+db_config = os.getenv("DB_CONFIG")
+db_password = os.getenv("DB_PASSWORD")
+print(f"initializing DB config: {db_config}")
+
+if db_config == sqlite:
+    SQL_ALCHEMY_DATABASE_URL = "sqlite:///./todo-service.db"
+    engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False, "timeout": 30},
+                           echo=True)
+elif db_config == postgres:
+    SQL_ALCHEMY_DATABASE_URL = f"postgresql://postgres:{db_password}@localhost:5432/TodoApplicationDatabase"
+    engine = create_engine(SQL_ALCHEMY_DATABASE_URL,
+                           echo=True)
+
 sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
